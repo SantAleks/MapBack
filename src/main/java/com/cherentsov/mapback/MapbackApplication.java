f25f3fca2441918af2a5856ecf78f1a58d2b6024
Черенцов Алексей <st_aleks@rambler.ru>
2019-01-20 20:06:03 +0700

    Начало настройки Spting

--- src/main/java/com/cherentsov/mapback/MapbackApplication.java
+++ src/main/java/com/cherentsov/mapback/MapbackApplication.java
@@ -1,18 +1,19 @@
 package com.cherentsov.mapback;
 
-import com.cherentsov.mapback.Model.*;
 import com.cherentsov.mapback.Service.DBService;
-import org.hibernate.HibernateException;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//@SpringBootApplication
 public class MapbackApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MapbackApplication.class, args);
-        DBService dbService = new DBService();
         System.out.println("fdgfg");
+        DBService dbService = new DBService();
         ExampleData.AddExampleToBD(dbService);
     }
 }
